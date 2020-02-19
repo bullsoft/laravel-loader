91053e809da5c5824cfecde6fb4e9a658409e103
Gu Weigang <guweigang@bullsoft.org>
2020-02-19 20:26:54 +0800

    initial project

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # laravel-loader
+奇怪的需求产生奇怪的方案，本方案就是。。。
+
+Load Laravel-Application in your codebase.
+
+```php
+$conf = [
+    'basePath' => '/path/to/your/laravel-app',
+    'url' => 'http://redirect/to/when/login/successfully',
+];
+
+$laravel = \BullSoft\Laravel::bootstrap($conf['basePath']);
+
+if(!$laravel->checkUserLogin()) {
+    $info = ['username' => 'roy', 'password' => 'iloveroy'];
+    $laravel->login($info, $conf['url']); // use null for second param when you do not want to redirect
+}
+
+$request  = $laravel->newApiRequest('/api/v1/user/index', 'GET');
+$response = $laravel->sendRequest($request);
+$content  = $response->getContent();
+$laravel->finish($request, $response);
+
+echo $content;
+```
---
+++ src/Loader/Laravel.php
@@ -0,0 +1,168 @@
+<?php
+namespace BullSoft\Loader;
+
+use Laravel\Passport\ApiTokenCookieFactory;
+use Illuminate\Support\Facades\Auth;
+
+class Laravel 
+{
+    /**
+     * @var \Illuminate\Foundation\Application
+     */
+    public $app;
+
+    /**
+     * @var \Illuminate\Foundation\Http\Kernel
+     */
+    public $kernel;
+
+    /**
+     * @var string
+     */
+    protected $sessionId;
+
+    /**
+     * @var \Illuminate\Http\Request
+     */
+    public $request; // ->duplicate()
+
+    public static function autoload(string $basePath)
+    {
+        require_once $basePath . '/bootstrap/autoload.php';
+    }
+
+    public static function bootstrap(string $basePath)
+    {
+        $laravel = new self($basePath);
+        $laravel->app->instance('request', $laravel->request->duplicate());
+        $laravel->kernel->bootstrap();
+        return $laravel;
+    }
+
+    private function __construct(string $basePath)
+    {
+        self::autoload($basePath);
+        $this->app = require_once $basePath. '/bootstrap/app.php';
+        $this->kernel = $this->app->make(\Illuminate\Contracts\Http\Kernel::class); 
+        $this->request = \Illuminate\Http\Request::capture();
+    }
+
+    public function getSessionId($request) : string
+    {
+        // SessionId
+        if(empty($this->sessionId)) {
+            $sessionId = $request->cookies->get(env('COOKIE_NAME'));
+            if(empty($sessionId)) {
+                session()->regenerate();
+                $sessionId = session()->getId(); // \Illuminate\Support\Str::random(40);
+            }
+            $this->sessionId = $sessionId;
+        }
+        return $this->sessionId;
+    }
+
+    protected function syncToken($request) : string
+    {
+        $sessionId = $this->getSessionId($request);
+        $handler = session()->getHandler();
+        $sesionArr = unserialize($handler->read($sessionId));
+        $token = $sesionArr['_token'] ?? null;
+        if(is_null($token)) {
+            session()->regenerateToken();
+            $token = session()->token();
+            $sesionArr['_token'] = $token;
+            $handler->write($sessionId, serialize($sesionArr));
+        }
+        return $token;
+    }
+
+    public function login(array $info, ?string $redirectUrl = null)
+    {
+        $request = $this->request->duplicate();
+        $token = $this->syncToken($request);
+        $info['token'] = $token;
+        $this->buildLoginRequest($request, $info);
+        $response = $this->sendRequest($request);
+        $this->finishWithCookies($request, $response, $redirectUrl);
+    }
+
+    protected function buildLoginRequest($request, array $info) : void
+    {
+        $userName  = $info['username'];
+        $password  = $info['password'];
+        $token     = $info['token'];
+        
+        $request->server->set('REQUEST_METHOD', 'POST');
+        $request->server->set('REQUEST_URI', '/login');
+        $request->request->set('username', $userName);
+        $request->request->set('password', $password);
+        $request->request->set('_token', $token);
+    
+        $sessionId = $this->getSessionId($request);
+        $request->cookies->set(env('COOKIE_NAME'), $sessionId);
+    }
+
+    public function checkUserLogin(string $uri = '/', $method = 'GET')
+    {
+        $request = $this->newRequest($uri, $method);
+        $response = $this->sendRequest($request);
+        $this->finish($request, $response);
+        return Auth::check();
+    }
+
+    public function newApiRequest(string $uri, string $method = 'GET')
+    {
+        $request = $this->request->duplicate();
+        $request->server->set('REQUEST_METHOD', strtoupper($method));
+        $request->server->set('REQUEST_URI', $uri);
+        $token = $this->syncToken($request);
+        $apiToken = new ApiTokenCookieFactory(app('config'), app('encrypter'));
+        $apiTokenCookie = $apiToken->make(Auth::id(), $token);
+        $request->headers->set('X-CSRF-TOKEN', $token);
+        $request->cookies->set($apiTokenCookie->getName(), encrypt($apiTokenCookie->getValue()));
+        $request->headers->set("X-Requested-With", 'XMLHttpRequest');
+        $sessionId = $this->getSessionId($request);
+        $request->cookies->set(env('COOKIE_NAME'), $sessionId);
+        return $request;
+    }
+
+    public function newRequest(string $uri, string $method = 'GET')
+    {
+        $request = $this->request->duplicate();
+        $request->server->set('REQUEST_METHOD', strtoupper($method));
+        $request->server->set('REQUEST_URI', $uri);
+        $token = $this->syncToken($request);
+        $request->request->set('_token', $token);
+        $sessionId = $this->getSessionId($request);
+        $request->cookies->set(env('COOKIE_NAME'), $sessionId);
+        return $request;
+    }
+
+    public function sendRequest($request)
+    {
+        return $this->kernel->handle($request);
+    }
+
+    public function finish($request, $response)
+    {
+        $this->kernel->terminate($request, $response);
+    }
+
+    public function finishWithCookies($request, $response, ?string $redirectUrl = null)
+    {
+        foreach ($response->headers->getCookies() as $cookie) {
+            if($cookie->getName() == env('COOKIE_NAME')) {
+                $this->sessionId = $cookie->getValue();
+            }
+            header('Set-Cookie: '.$cookie, false, 200);
+            $this->request->cookies->set($cookie->getName(), $cookie->getValue());
+        }
+        if(!empty($redirectUrl)) {
+            header("Location: {$redirectUrl}", true, 302);
+        } else {
+            header(sprintf('HTTP/%s %s %s', '1.1', "200", "OK"), true, 200);
+            return $response->getContent();
+        }
+        $this->kernel->terminate($request, $response);
+    }
+}
